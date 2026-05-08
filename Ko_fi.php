0b8f4f16dcd322f37d68c7e44f06aaf92c337dda
Cameron Jones <cameron@cameronjonesweb.com.au>
2026-05-08 16:49:02 +0100

    Hotfix the other place the version needs to be updated

--- Ko_fi.php
+++ Ko_fi.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ko-fi Button
  * Plugin URI:
  * Description: A Ko-fi donate button for your website!
- * Version: 1.3.10
+ * Version: 1.4.0
  * Author: Ko-fi Team
  * Author URI: https://www.ko-fi.com
  * License: GPL2
